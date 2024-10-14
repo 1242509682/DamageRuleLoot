d3e1b447b6a89fa3b9ddd16fc238f849cd538787
羽 学 <1242509682@qq.com>
2024-10-14 17:31:06 +0800

    添加项目文件。

---
+++ Configuration.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using TShockAPI;
+
+namespace Plugin
+{
+    internal class Configuration
+    {
+        #region 实例变量
+        [JsonProperty("插件开关", Order = -8)]
+        public bool Enabled { get; set; } = true;
+
+        [JsonProperty("领取条件/百分比", Order = -8)]
+        public double Damages { get; set; } = 0.3;
+
+        [JsonProperty("伤害统计播报", Order = -8)]
+        public bool Broadcast { get; set; } = true;
+
+        [JsonProperty("物品ID", Order = -2)]
+        public int[] ItemID { get; set; } = new int[] { 3318,3319,3320,3321,3322,3323,3324,3325,3326,3327,3328,3329,3330,3331,3332,3860,4782,4957,5111};
+
+        [JsonProperty("玩家数据表", Order = 4)]
+        public List<ItemData> Items { get; set; } = new List<ItemData>();
+        #endregion
+
+        #region 预设参数方法
+        public void Ints()
+        {
+            Items = new List<ItemData>
+            {
+                new ItemData("羽学",true,0,0)
+            };
+        }
+        #endregion
+
+        #region 数据结构
+        public class ItemData
+        {
+            [JsonProperty("玩家名字", Order = 1)]
+            public string Name { get; set; }
+            [JsonProperty("领取条件", Order = 2)]
+            public bool Enabled { get; set; }
+            [JsonProperty("伤害值", Order = 3)]
+            public double Damage { get; set; }
+            [JsonProperty("伤害百分比", Order = 4)]
+            public double Damage2 { get; set; }
+
+            public ItemData(string name = "", bool enabled = false, double damage = 0, double damage2 = 0)
+            {
+                Name = name ?? "";
+                Enabled = enabled;
+                Damage = damage;
+                Damage2 = damage2;  
+            }
+        }
+        #endregion
+
+        #region 读取与创建配置文件方法
+        public static readonly string FilePath = Path.Combine(TShock.SavePath, "伤害规则掉落.json");
+
+        public void Write()
+        {
+            string json = JsonConvert.SerializeObject(this, Formatting.Indented); 
+            File.WriteAllText(FilePath, json);
+        }
+
+        public static Configuration Read()
+        {
+            if (!File.Exists(FilePath))
+            {
+                var NewConfig = new Configuration();
+                new Configuration().Write();
+                return NewConfig;
+            }
+            else
+            {
+                string jsonContent = File.ReadAllText(FilePath);
+                return JsonConvert.DeserializeObject<Configuration>(jsonContent)!;
+            }
+        }
+        #endregion
+
+
+
+    }
+}
---
+++ DamageRuleLoot.cs
@@ -0,0 +1,159 @@
+﻿using System.Text;
+using Terraria;
+using Terraria.ID;
+using TerrariaApi.Server;
+using TShockAPI;
+using TShockAPI.Hooks;
+using Microsoft.Xna.Framework;
+
+
+namespace Plugin
+{
+    [ApiVersion(2, 1)]
+    public class DamageRuleLoot : TerrariaPlugin
+    {
+
+        #region 插件模版信息
+        public override string Name => "伤害规则掉落";
+        public override string Author => "羽学";
+        public override Version Version => new Version(1, 0, 0);
+        public override string Description => "涡轮增压不蒸鸭";
+        #endregion
+
+        #region 注册与释放
+        public DamageRuleLoot(Main game) : base(game) { }
+        private GeneralHooks.ReloadEventD _reloadHandler;
+        public override void Initialize()
+        {
+            LoadConfig();
+            this._reloadHandler = (_) => LoadConfig();
+            GeneralHooks.ReloadEvent += this._reloadHandler;
+            ServerApi.Hooks.NpcSpawn.Register(this, this.OnNpcSpawn);
+            ServerApi.Hooks.NpcStrike.Register(this, this.OnStrike);
+            ServerApi.Hooks.NpcKilled.Register(this, this.OnNpcKill);
+            ServerApi.Hooks.ServerJoin.Register(this, OnJoin);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                GeneralHooks.ReloadEvent -= this._reloadHandler;
+                ServerApi.Hooks.NpcSpawn.Deregister(this, this.OnNpcSpawn);
+                ServerApi.Hooks.NpcStrike.Deregister(this, this.OnStrike);
+                ServerApi.Hooks.NpcKilled.Deregister(this, this.OnNpcKill);
+                ServerApi.Hooks.ServerJoin.Deregister(this, OnJoin);
+                this.DamageList.Clear();
+            }
+            base.Dispose(disposing);
+        }
+        #endregion
+
+        #region 配置重载读取与写入方法
+        internal static Configuration Config = new();
+        private static void LoadConfig()
+        {
+            Config = Configuration.Read();
+            Config.Write();
+            TShock.Log.ConsoleInfo("[伤害规则掉落]重新加载配置完毕。");
+        }
+        #endregion
+
+        #region 玩家更新配置方法（创建配置结构）
+        private void OnJoin(JoinEventArgs args)
+        {
+            var plr = TShock.Players[args.Who];
+            var list = Config.Items.FirstOrDefault(x => x.Name == plr.Name);
+
+            if (!Config.Enabled) return;
+            if (!Config.Items.Any(item => item.Name == plr.Name))
+            {
+                Config.Items.Add(new Configuration.ItemData()
+                {
+                    Name = plr.Name,
+                    Enabled = false,
+                    Damage = 0,
+                    Damage2 = 0
+                });
+            }
+            else
+            {
+                list!.Enabled = false;
+                list.Damage = 0;
+                list.Damage2 = 0;
+            }
+            Config.Write();
+        }
+        #endregion
+
+        #region 伤害统计方法
+        private readonly Dictionary<NPC, Dictionary<string, double>> DamageList = new Dictionary<NPC, Dictionary<string, double>>();
+        private void OnNpcSpawn(NpcSpawnEventArgs args)
+        {
+            if (!Config.Enabled) return;
+            var npc = Main.npc[args.NpcId];
+            if (npc.boss)
+            {
+                this.DamageList.Add(npc, new Dictionary<string, double>());
+            }
+        }
+        private void OnStrike(NpcStrikeEventArgs args)
+        {
+            if (!Config.Enabled) return;
+            if (this.DamageList.ContainsKey(args.Npc))
+            {
+                if (!this.DamageList[args.Npc].ContainsKey(args.Player.name))
+                {
+                    this.DamageList[args.Npc].Add(args.Player.name, 0);
+                }
+                this.DamageList[args.Npc][args.Player.name] += args.Damage;
+            }
+        }
+        private void OnNpcKill(NpcKilledEventArgs args)
+        {
+            if (!Config.Enabled) return;
+            if (this.DamageList.ContainsKey(args.npc) && this.DamageList[args.npc].Any())
+            {
+                var data = this.DamageList[args.npc];
+                double npcLifeMax = 0;
+                data.ForEach(p => npcLifeMax += data[p.Key]);
+                var text = new StringBuilder();
+                data.Keys.ForEach(p => text.AppendLine($"{p}: [c/74F3C9:{data[p]}] <{data[p] / npcLifeMax:0.00%}>, "));
+
+                if (Config.Broadcast)
+                {
+                    TShock.Utils.Broadcast($"[c/74F3C9:{data.Count}] 位玩家击败了 [c/74F3C9:{args.npc.FullName}]\n{text}", new Color(247, 244, 150));
+                }
+
+                foreach (var plr in TShock.Players.Where(plr => plr != null && plr.Active && plr.IsLoggedIn))
+                {
+                    var Damage = data.FirstOrDefault(p => p.Key == plr.Name);
+                    var item = Config.Items.FirstOrDefault(x => x.Name == plr.Name);
+                    if (item != null)
+                    {
+                        item.Damage = Damage.Value;
+                        item.Damage2 = Damage.Value / npcLifeMax;
+                        item.Enabled = item.Damage2 >= Config.Damages;
+                        Config.Write();
+                    }
+
+                    if (item != null && item.Damage2 < Config.Damages)
+                    {
+                        for (int i = 0; i < Terraria.Main.maxItems; i++)
+                        {
+                            var item2 = Terraria.Main.item[i];
+
+                            if (Config.ItemID.Contains(item2.netID))
+                            {
+                                Terraria.Main.item[i].active = false;
+                                plr.SendData(PacketTypes.ItemDrop, "", i);
+                            }
+                        }
+                    }
+                }
+                this.DamageList.Remove(args.npc);
+            }
+        }
+        #endregion
+
+    }
+}
